3605cb5a6fe715c973182d6c8a401cd5a32bf728
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-04-18 17:02:37 +0200

    allImages list ok

--- static/js/game.js
+++ static/js/game.js
@@ -1,30 +1,24 @@
 var numOfCells = document.getElementsByClassName("cells").length;
 
-var allImages = new Array("fab fa-500px", "fas fa-address-book", "fab fa-amazon", 
-                          "fas fa-adjust", "fab fa-affiliatetheme", "fab fa-android", 
-                          "fas fa-anchor", "fas fa-arrows-alt", "fas fa-balance-scale", 
+var allImages = new Array("fas fa-signal", "fas fa-address-book", "fas fa-car", "fas fa-code-branch", 
+                          "fas fa-adjust", "fas fa-box-open", "fas fa-anchor", "fas fa-compass", 
+                          "fas fa-anchor", "fas fa-arrows-alt", "fas fa-balance-scale", "fas fa-hand-holding-usd",
                           "fas fa-basketball-ball", "fas fa-at", "fas fa-barcode", "fas fa-bath", 
-                          "fas fa-battery-three-quarters", "fas fa-bicycle", "fab fa-chrome", 
-                          "fas fa-futbol", "fas fa-heart", "fab fa-internet-explorer", "fas fa-save",
-                          "fas fa-moon", "fas fa-music");
+                          "fas fa-battery-three-quarters", "fas fa-bicycle", "fas fa-home",
+                          "fas fa-futbol", "fas fa-heart", "fas fa-save", "fas fa-road", "fas fa-table-tennis",
+                          "fas fa-moon", "fas fa-music", "fas fa-phone", "fas fa-search",
+                          "fas fa-shower", "fas fa-sun", "fas fa-subway", "fas fa-thermometer-half");
 
+document.getElementById('a').innerHTML = allImages.length;/* for test only, needed 32 */
 
 var imagesArray = allImages.slice(0, numOfCells - 1); /* finally numOfCells-1/2 */
 
-/*
 for (i=0; i<=numOfCells-1; i++) {
-    i = string(i);
-    document.getElementById(i+'t').classList.add("fas", "fa-moon");
-    document.getElementById(i+'t').classList.add("fas", "fa-align-justify");
+    document.getElementById(i+'i').classList.add("fas", "fa-align-justify");
 };
-*/
 
-function change(id) {
+
+function togClass(id) {
     var cell = document.getElementById(id);
-    if (cell.className === "fas fa-align-justify") {
-        cell.classList.add("fas", "fa-moon");
-    } else if (cell.className === "fas fa-align-justify fa-moon") {
-        cell.classList.remove("fa-moon");
-        /*cell.classList.add("fas", "fa-align-justify");*/
-    }
+    cell.classList.toggle();
 };
